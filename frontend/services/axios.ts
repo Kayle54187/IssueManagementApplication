db266b3a2b7e7a5c55f4889caab38d4db70685df
Kayle54187 <habichristi@gmail.com>
2024-09-04 14:10:32 +0200

    feat: configuring axios

---
+++ frontend/services/axios.ts
@@ -0,0 +1,27 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+
+const baseInstance = axios.create({
+	baseURL: "localhost:8000",
+});
+
+function baseRequestSuccessResponseInterceptor(response: AxiosResponse) {
+	return response;
+}
+
+function baseRequestErrorResponseInterceptor(error: AxiosError) {
+	const status = error.response?.status;
+	const url = error.request.responseURL;
+
+	if (status === 401) {
+		console.log("401", url);
+		window.location.href = "/sign-in";
+	}
+	return Promise.reject(error);
+}
+
+baseInstance.interceptors.response.use(
+	baseRequestSuccessResponseInterceptor,
+	baseRequestErrorResponseInterceptor
+);
+
+export { baseInstance };
