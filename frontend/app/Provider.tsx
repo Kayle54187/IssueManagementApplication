681d70c44d489b739665e4d86e9749668be5639d
Kayle54187 <habichristi@gmail.com>
2024-09-04 14:15:07 +0200

    feat: configuring tanstack/react-query

---
+++ frontend/app/Provider.tsx
@@ -0,0 +1,16 @@
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React from "react";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+function Provider({ children }: { children: React.ReactNode }) {
+	const queryClient = new QueryClient();
+
+	return (
+		<QueryClientProvider client={queryClient}>
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	);
+}
+
+export default Provider;
